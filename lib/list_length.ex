fb5e30e778546bf0e04e9594e263ed3649c384d9
Ruan Barros <ruan.victor21052000@gmail.com>
2022-12-08 17:12:19 -0300

    feat: add recursive length

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,15 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    length(list, 0)
+  end
+
+  defp length([], acc) do
+    acc
+  end
+
+  defp length(list, acc) do
+    [_head | tail] = list
+
+    length(tail, acc + 1)
   end
 end
